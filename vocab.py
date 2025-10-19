afa66b57bab1067bde27cfc7a04e58010e200b8b
Ashton Dias <143654086+Ashton-Dias@users.noreply.github.com>
2025-10-19 11:37:11 -0400

    Implement Vocabulary class for word management

---
+++ vocab.py
@@ -0,0 +1,33 @@
+from vocab import Vocabulary
+import pickle
+
+
+class Vocabulary:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "PAD", 1: "SOS", 2: "EOS"}
+        self.num_words = 3
+        
+    def add_sentence(self, sentence):
+        for word in sentence.split():
+            self.add_word(word)
+    
+    def add_word(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+    
+    def trim_rare_words(self, min_count=3):
+        keep_words = [word for word, count in self.word2count.items() if count >= min_count]
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "PAD", 1: "SOS", 2: "EOS"}
+        self.num_words = 3
+        for word in keep_words:
+            self.add_word(word)
